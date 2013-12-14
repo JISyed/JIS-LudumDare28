7323f5ade9d08c5ae2f178cccd8f12f33e771d7a
Jibran Syed <jisyed14@gmail.com>
2013-12-14 15:35:49 -0600

    Added scale vector and euler angles as a vector. Data also stored in matrices for graphics purposes.

--- VSProject/OpenGL_SDL_Guide/GameObject.cpp
+++ VSProject/OpenGL_SDL_Guide/GameObject.cpp
@@ -13,6 +13,9 @@ GameObject::GameObject()
 {
 	this->position = glm::vec3(0.0f, 0.0f, 0.0f);
 	this->textureUnit = 0;
+	this->rotationMatrix = glm::mat4(1.0f);
+	this->translateMatrix = glm::translate(this->position);
+	this->scaleMatrix = glm::mat4(1.0f);
 }
 
 // Destructor
@@ -27,6 +30,8 @@ GameObject::~GameObject()
 void GameObject::Update()
 {
 
+	// Update internal matrix data
+	this->updateMatrices();
 }
 
 // Draw the object
@@ -47,6 +52,26 @@ void GameObject::SetPosition(float newX, float newY, float newZ)
 	this->position = glm::vec3(newX, newY, newZ);
 }
 
+void GameObject::SetScale(glm::vec3 newScale)
+{
+	this->scale = newScale;
+}
+
+void GameObject::SetScale(float newX, float newY, float newZ)
+{
+	this->scale = glm::vec3(newX, newY, newZ);
+}
+
+void GameObject::SetEulerAngles(glm::vec3 newEulerAngles)
+{
+	this->eulerAngles = newEulerAngles;
+}
+
+void GameObject::SetEulerAngles(float xRotation, float yRotation, float zRotation)
+{
+	this->eulerAngles = glm::vec3(xRotation, yRotation, zRotation);
+}
+
 void GameObject::SetTextureUnit(unsigned int newTextureUnit)
 {
 	this->textureUnit = newTextureUnit;
@@ -57,8 +82,38 @@ glm::vec3 GameObject::GetPosition()
 	return this->position;
 }
 
+glm::vec3 GameObject::GetScale()
+{
+	return this->scale;
+}
+
+glm::vec3 GameObject::GetEulerAngles()
+{
+	return this->eulerAngles;
+}
+
 unsigned int GameObject::GetTextureUnit()
 {
 	return this->textureUnit;
 }
 
+//  Helpers ===========================================
+
+void GameObject::updateMatrices()
+{
+	this->translateMatrix = glm::translate(this->position);
+	this->scaleMatrix = glm::scale(this->scale);
+
+	this->rotationMatrix = glm::rotate(eulerAngles.x, 1.0f, 0.0f, 0.0f);
+	this->rotationMatrix = glm::rotate(this->rotationMatrix, eulerAngles.y, 0.0f, 1.0f, 0.0f);
+	this->rotationMatrix = glm::rotate(this->rotationMatrix, eulerAngles.z, 0.0f, 0.0f, 1.0f);
+}
+
+glm::mat4 GameObject::constructModelMatrix()
+{
+	glm::mat4 modelMatrix;
+
+	modelMatrix = this->translateMatrix * this->rotationMatrix * this->scaleMatrix;
+
+	return modelMatrix;
+}
--- VSProject/OpenGL_SDL_Guide/GameObject.h
+++ VSProject/OpenGL_SDL_Guide/GameObject.h
@@ -25,16 +25,34 @@ public:
 
 	void SetPosition(glm::vec3 newPosition);
 	void SetPosition(float newX, float newY, float newZ);
+	void SetScale(glm::vec3 newScale);
+	void SetScale(float newX, float newY, float newZ);
+	void SetEulerAngles(glm::vec3 newEulerAngles);
+	void SetEulerAngles(float xRotation, float yRotation, float zRotation);
 	void SetTextureUnit(unsigned int newTextureUnit);
 
 	glm::vec3 GetPosition();
+	glm::vec3 GetScale();
+	glm::vec3 GetEulerAngles();
 	unsigned int GetTextureUnit();
 
 private:
 
 	// Data ========================
 	glm::vec3 position;					// Position in world space
+	glm::vec3 scale;					// Mesh scale in 3 dimensions
+	glm::vec3 eulerAngles;				// Angle rotations in 3 dimentions
 	unsigned int textureUnit;			// Assigns a texture unit to an object
+
+	// Matrices ====================
+	glm::mat4 rotationMatrix;
+	glm::mat4 translateMatrix;
+	glm::mat4 scaleMatrix;
+
+	// Helpers =====================
+
+	void updateMatrices();				// Update all tranformation matrices
+	glm::mat4 constructModelMatrix();	// Make the model matrix to send to shaders
 };
 
 
